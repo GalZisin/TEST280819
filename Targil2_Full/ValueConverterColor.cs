a392b5bad1f09fd493549ce1857ca7d84325d030
GalZisin <46171488+GalZisin@users.noreply.github.com>
2019-09-02 03:18:36 +0300

    Add files via upload

---
+++ Targil2_Full/ValueConverterColor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Targil2
+{
+    class ValueConverterColor : IValueConverter
+    {
+        public Brush NormalBrush { get; set; }
+
+        public Brush HighlightBrush { get; set; }
+       
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Brush brush = null;
+            if (value == null)
+                return NormalBrush;
+
+           int number = System.Convert.ToInt32(value.ToString());
+            if (number>15)
+            {
+                brush= NormalBrush;
+            }
+            else if(number<=15)
+            {
+                brush = HighlightBrush;
+            }
+            return brush;
+        }
+         
+ 
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
